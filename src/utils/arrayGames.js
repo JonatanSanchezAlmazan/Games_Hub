adc52fb8a879d1c031bd0cd53f81a7b251f344f8
Jonatan <jonatansan1@hotmail.com>
2024-03-06 11:58:53 +0100

    new commit

--- src/utils/arrayGames.js
+++ src/utils/arrayGames.js
@@ -1,16 +1,16 @@
 export const arrayGames = [{
         name: "Whac-a-mole",
-        img: "/public/assets/bgWhac.png",
+        img: "/assets/bgWhac.png",
         description: "Trata de golpear los topos cuando vayan saliendo de sus madrigueras!! El juego empieza con una complejidad simple pero a medida que vas consiguiendo puntos los topos van apareciendo más seguidos, serás capaz de ganar muchos puntos? Adelante entra y no te lo pierdas."
     },
     {
         name: "Piedra Papel o Tijera",
-        img: "/public/assets/bgPpt.png",
+        img: "/assets/bgPpt.png",
         description: "Trata de derrotar a la máquina jugando al juego de Piedra Papel o Tijera."
     },
     {
         name: "Memory Game",
-        img: "/public/assets/bgMemory.webp",
+        img: "/assets/bgMemory.webp",
         description: "Trata de encontrar todas las parejas de los personajes de la patrulla canina para poder completar el juego."
     },
 
